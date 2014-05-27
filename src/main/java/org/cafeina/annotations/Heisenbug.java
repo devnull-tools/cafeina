c0431154b43eb528195d8e17acb118371225510d
Ataxexe <ataxexe@gmail.com>
2014-05-27 17:16:53 -0300

    copyright

--- src/main/java/org/cafeina/annotations/Heisenbug.java
+++ src/main/java/org/cafeina/annotations/Heisenbug.java
@@ -1,7 +1,8 @@
 /*
  * The MIT License
  *
- *        Copyright (c) 2014 Marcelo Guimaraes <ataxexe@gmail.com>
+ * Copyright (c) 2014 Marcelo Guimaraes <ataxexe@gmail.com>
+ *
  * ----------------------------------------------------------------------
  * Permission  is hereby granted, free of charge, to any person obtaining
  * a  copy  of  this  software  and  associated  documentation files (the
